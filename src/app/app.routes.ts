118bb3e901d19af81af9d41b5c7a12ed46f4cf18
gurdiano <tiagofran2013@hotmail.com>
2025-07-25 16:49:25 -0300

    initialize project with routes and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: Home},
+    {path: 'skills', component: Home},
+    {path: 'projects', component: Home},
+    {path: 'about', component: Home},
+    {path: 'contact', component: Home},
+];

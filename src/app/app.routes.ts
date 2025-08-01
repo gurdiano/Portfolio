e722283da6e2890e3c33531b442cf5b2a8722d77
gurdiano <tiagofran2013@hotmail.com>
2025-08-01 10:53:15 -0300

    create about-page with temporary text variables

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: Home},
     {path: 'skills', component: Home},
     {path: 'projects', component: Home},
-    {path: 'about', component: Home},
+    {path: 'about', component: About},
     {path: 'contact', component: Home},
 ];

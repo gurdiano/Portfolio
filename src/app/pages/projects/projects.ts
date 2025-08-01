11ff4c96d5fd78eb50ec8692cee673d96d9ce745
gurdiano <tiagofran2013@hotmail.com>
2025-08-01 19:14:51 -0300

    Create projects-page

---
+++ src/app/pages/projects/projects.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { BaseCard } from '../../components/base-card/base-card';
+
+@Component({
+  selector: 'app-projects',
+  imports: [
+    BaseCard,
+  ],
+  templateUrl: './projects.html',
+  styleUrl: './projects.css'
+})
+export class Projects {
+
+}

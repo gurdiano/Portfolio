f36a3cdcc68136f0312f7a50b794eaa145f5e085
gurdiano <tiagofran2013@hotmail.com>
2025-08-07 13:57:31 -0300

    add specific backgroung for main page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,17 @@ import { Footer } from './components/footer/footer';
     RouterOutlet,
     Navbar,
     Footer,
+    NgClass,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Portfolio');
+
+  router = inject(Router);
+
+  isHome(): boolean {
+    return this.router.url == '/home'
+  }
 }
